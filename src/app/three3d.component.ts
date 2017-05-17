ffb3b7c4cc61491e91d9c46526a63629648d0741
Shaun Savage <savages@savages.com>
2017-05-17 03:08:34 +0000

    infoBoc popup works

--- src/app/three3d.component.ts
+++ src/app/three3d.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three-orbitcontrols-ts';
@@ -18,7 +18,7 @@ export class Three3dComponent implements OnInit {
     element: HTMLElement;
     infoBox: HTMLElement;
 
-    focusOut: EventEmitter<any>;
+    @Output() focusOut: EventEmitter<any>;
     focusIn: EventEmitter<any>;
     chgValue: EventEmitter<any>;
 
@@ -26,14 +26,24 @@ export class Three3dComponent implements OnInit {
     renderer: THREE.WebGLRenderer;
     scene: THREE.Scene;
     grid: HexGrid;
-    screenPosition = new THREE.Vector2(-1,-1);
+    screenPosition = this.service3d.screenPosition;
     width: number;
     height: number;
     top: number;
     camera: THREE.PerspectiveCamera;
     orbit: OrbitControls;
+    center3d: any;
 
     @Input() appObj: AppComponent;
+
+    //infoBox stuff
+    ib_id: number;
+    ib_r: number;
+    ib_q: number;
+    ib_s: number;
+    ib_x: number;
+    ib_y: number;
+    ib_zoom: string;
 
     constructor(private service3d: Three3dService) {
         this.sceneSettings = {
@@ -67,7 +77,6 @@ export class Three3dComponent implements OnInit {
             opacity: 100
         });
         //this.userEvt = new Subject();
-        this.screenPosition = new THREE.Vector2(-1,-1);
 
         this.grid = new HexGrid();
 
@@ -82,12 +91,22 @@ export class Three3dComponent implements OnInit {
     ngAfterViewInit() {
         let ele = document.getElementById("three3d");
         this.element = ele;
+        this.service3d.element = ele;
         let width = ele.clientWidth;
         let height = ele.clientHeight;
 
         this.width = ele.clientWidth;
         this.height = ele.clientHeight;
         this.top = ele.offsetTop;
+        this.center3d = {'x':width/2, 'y':height/2+this.top};
+
+        this.focusOut = this.service3d.focusOut;
+        this.focusIn = this.service3d.focusIn;
+        this.chgValue = this.service3d.chgValue;
+
+        this.focusIn.subscribe(hov => this.inCell(hov));
+        this.focusOut.subscribe(hov => this.outCell(hov));
+        this.chgValue.subscribe(hov => this.chgCell(hov));
 
         this.camera = new THREE.PerspectiveCamera( 70, width / height, 1, 3000 );
         this.camera.position.y = -350;
@@ -102,13 +121,40 @@ export class Three3dComponent implements OnInit {
         this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
         this.service3d.orbit = this.orbit;
 
-        this.grid.build(1)
+        this.grid.build(6)
         this.grid.grid.visible = true;
         this.scene.add(this.grid.grid);
 
         this.service3d.update();
     }
 
+    inCell(hov) {
+        let xy = this.grid.cell2Screen(hov);
+        let scale = 1/this.orbit.totalScale;
+        xy = {'x': xy.x*scale + this.center3d.x, 'y': xy.y*scale + this.center3d.y};
+        console.log("IN ",hov.uniqueID, xy, this.orbit.totalScale, this.service3d.mouseLoc);
+        this.infoBox.style.top = xy.y.toString()+"px";
+        this.infoBox.style.left = xy.x.toString()+"px";
+        this.ib_id = hov.uniqueID;
+        this.ib_q = hov.q;
+        this.ib_r = hov.r;
+        this.ib_s = hov.s;
+        this.ib_x = xy.x;
+        this.ib_y = xy.y;
+        this.ib_zoom = scale.toFixed(4);
+        this.infoBox.style.visibility = "visible";
+    }
+
+    outCell(hov) {
+        this.infoBox.style.visibility = "hidden";
+
+        console.log("OUT ",hov.uniqueID);
+
+    }
+
+    chgCell(hov) {
+        console.log("CHG ",hov.uniqueID);
+    }
 
     buildGrid(conf) {
         this.grid.load(conf)
--- src/app/three3d.service.ts
+++ src/app/three3d.service.ts
@@ -17,6 +17,15 @@ export class Three3dService {
     _render: any;
     _scene: THREE.Scene;
     _camera: THREE.PerspectiveCamera;
+    _element: HTMLElement;
+    raycaster = new THREE.Raycaster();
+    @Output() screenPosition = new THREE.Vector3;
+    hoverObject: any;
+    selectedObject: any;
+    width: number;
+    height: number;
+    top: number;
+    mouseLoc: any;  // debug hex 2 screen
 
     @Output() focusOut: EventEmitter<any>;
     @Output() focusIn: EventEmitter<any>;
@@ -40,9 +49,157 @@ export class Three3dService {
         this._render = rend;
     }
 
-    setupListener() {
+    @Input() set element(ele: HTMLElement) {
+        this._element = ele;
+        this.width = ele.clientWidth;
+        this.height = ele.clientHeight;
+        this.top = ele.offsetTop;
+
+        this.focusIn = new EventEmitter();
+        this.focusOut = new EventEmitter();
+        this.chgValue = new EventEmitter();
+
+        this._element.addEventListener('keydown', this.userEventHandler.bind(this), true);
+        this._element.addEventListener('mousedown', this.userEventHandler.bind(this), true);
+        this._element.addEventListener('contextmenu', this.userEventHandler.bind(this), false);
+        this._element.addEventListener('wheel', this.userEventHandler.bind(this), true);
+        this._element.addEventListener('mouseup', this.userEventHandler.bind(this), true);
+        this._element.addEventListener('mousemove', this.userEventHandler.bind(this), true);
+    }
+
+    userEventHandler( evt: Event) {
+        switch(evt.type) {
+        case 'wheel':
+            this.wheelHandler(evt);
+            break;
+        case 'mousemove':
+            this.mouseMoveHandler(evt);
+            break;
+        case 'keydown':
+            this.keyDownHandler(evt);
+            break;
+        case 'mousedown':
+            this.mouseDownHandler(evt);
+            console.log("mousedown");
+            break;
+        case 'mouseup':
+            console.log('mouseup')
+            break;
+        case 'contextmenu':
+            console.log('contextmenu');
+            break;
+        default:
+            console.log("default user event");
+        }
+    }
+
+    keyDownHandler(evt: Event) {
+        console.log("keyDown");
+        return false;
+    }
+
+    mouseMoveHandler(evt: any) {
+        this.mouseLoc = {'x':evt.clientX, 'y': evt.clientY};
+        this.screenPosition.x = (evt.clientX / this.width) * 2 - 1;
+        this.screenPosition.y = -((evt.clientY - this.top) / this.height) * 2 + 1;
+    }
+
+    wheelHandler(evt: any) {
+        console.log("wheel");
+        if ( evt.shiftKey ) {
+            //console.log("W");
+            let hover = this._render.hoverObject.extdMesh;
+            if (hover) {
+                let top = hover.children[1];
+                let cell = hover.children[0];
+                top.material.color.setHex(0x00cc00);
+                hover.cell.grow(-evt.deltaY);
+                //removeTile(this.pickedObject);
+                //addTile(cel);
+                //this.pickedObject.cell.h += evt.deltaY;
+                //this.box.parameters.height += 0.1 * evt.deltaY;
+
+                evt.stopPropagation();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    mouseDownHandler(evt: any) {
+        let render = this._render;
+        if (render.hoverObject.children) {
+            let mesh = render.hoverObject.children[1];
+            if (evt.button === 0) {
+                if (render.selectedObject) {
+                    mesh.material.color.setHex(0xff0000);
+                    render.selectedObject = null;
+                } else {
+                    render.selectedObject = render.hoverObject;
+                    mesh.material.color.setHex(0x00cc00);
+                }
+            }
+
+            if (evt.button === 2 && render.selectedObject) {
+                mesh.material.color.setHex(0x444444);
+            }
+        }
+
+        render.update();
+        console.log("mouseDown");
+        return false;
+    }
+
+    ctlupdate() {
+        let raycaster = this.raycaster;
+        let screenPosition = this.screenPosition;
+        if (isNaN(screenPosition.x) || isNaN(screenPosition.y)
+    /*|| screenPosition.x < 0 || screenPosition.y < 0*/ ) {
+            //console.log(screenPosition.x, screenPosition.y);
+            return;
+        }
+        //screenPosition.y = this.screenPosition.y - this.element.offsetTop / 2;
+        raycaster.setFromCamera(this.screenPosition, this._camera);
+    //                var intersects = this.raycaster.intersectObject(this.group, true);
+        let scene = this._scene;
+        let intersects = raycaster.intersectObject(scene, true);
+        let hov = this.hoverObject;
+        let sel = this.selectedObject;
+
+        if (intersects.length > 0) {
+            // change each intersect object
+            // looks like 6, 4 lines and 2 mesh
+            let object: any = intersects[0].object;
+            let group = object.group;
+                    //let mesh: any = group.children[1];
+            let cell = group.cell;
+            if (hov && hov != cell ) {
+                // was hover now not
+                //focusout event to cell.
+                this.focusOut.emit(hov);
+                hov.focusout();
+                cell.focusin();
+                this.hoverObject = cell;
+                this.focusIn.emit(cell);
+            }
+            if (!hov) {
+                cell.focusin();
+                this.focusIn.emit(cell);
+                this.hoverObject = cell;
+            }
+            if (hov == cell) {
+                return;
+            }
+        }
+        if (hov) {
+            hov.focusout();
+            this.focusOut.emit(hov);
+            this.hoverObject = null;
+        }
+
 
     }
+
 
     update() {
         this.animate();
@@ -51,7 +208,7 @@ export class Three3dService {
     private animate = () => {
         requestAnimationFrame(this.animate);
         this._orbit.update();
-//        this.component.userEvents.update(this);
+        this.ctlupdate();
         this._render.render( this._scene, this._camera);
     }
 }
